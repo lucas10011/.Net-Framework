b302900be9a4ec4c876fd31d24c574f847059d41
lucas10011 <56743482+lucas10011@users.noreply.github.com>
2022-01-07 06:56:06 -0300

    Add files via upload

---
+++ 60-Interfaces/Framework.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SistemaNotificacao
+{
+    public interface INotificacao
+    {
+        void Enviar(string Mensagem);
+    }
+
+    public abstract class Notificacao: INotificacao
+    {
+        public abstract void Enviar(string Mensagem);
+    }
+
+    public class EMail : Notificacao, INotificacao
+    {
+        public override void Enviar(string Mensagem)
+        {
+            Console.WriteLine("Enviando EMAIL: " + Mensagem);
+        }
+    }
+
+    public class SMS : Notificacao, INotificacao
+    {
+        public override void Enviar(string Mensagem)
+        {
+            Console.WriteLine("Enviando SMS: " + Mensagem);
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome;
+    }
+
+    public class Funcionario : Pessoa, INotificacao
+    {
+        public void Enviar(string Mensagem)
+        {
+            Console.WriteLine("Pessoa está notificando: " + Mensagem);
+        }
+    }
+
+}
---
+++ 60-Interfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaNotificacao
+{
+    // UI
+    class Program
+    {
+        // associação concreta
+        //private static EMail email = new EMail();
+        //private static SMS sms = new SMS();
+        // associação para uma abstração
+        private static INotificacao notificacao;
+ 
+        static void Main(string[] args)
+        {
+            string Mensagem = "Ola Mundo";
+            notificacao = new EMail();
+            notificacao.Enviar(Mensagem); // delegação
+            Console.ReadLine();
+        }
+    }
+}
