e46d5ca4a533f5f98aa99c1b971855d50662bb69
lucas10011 <56743482+lucas10011@users.noreply.github.com>
2022-01-07 06:59:29 -0300

    Add files via upload

---
+++ 64-Introdução a Lambda/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lambda
+{
+    public delegate void PagarNotify (double valor);
+    public class Pedido
+    {
+        // PagarEvent é um ponteiro para um método e PagarNotify é o tipo de parametro que esse Ponteiro vai receber
+        // forma de injetar código no meu framework
+        // código da implementação vem de fora
+        public event PagarNotify PagarEvent; // Vai me apontar para algum metodo que seja do msm tipo de PagarNotify, ou seja recebe um valor double
+        public void Pagar(double valor)
+        {
+            //Na hora que for pagar ela chama o evento PagarEvent mas ele nao me diz como é que paga, ele ta chamando o ponteiro para um metodo
+            // delegação
+            //quando estou chamando esse metodo na vdd estaria chamando esse aqui no programcs new PagarNotify(PagarBoleto); ou a expressao lambda
+            this.PagarEvent(valor);         
+        }
+    }
+   
+}
---
+++ 64-Introdução a Lambda/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 2500;
+            var pedido = new Pedido();
+            //Pedido eu sei que vc tem um evento que vc vai chamar pois vc é um ponteiro, e pra qm vc vai apontar esse evento
+            // pedido.PagarEvent += new PagarNotify(PagarBoleto);
+            pedido.PagarEvent += v => Console.WriteLine("Pago no cartão valor de " + v);
+            pedido.Pagar(valor);
+            Console.ReadLine();
+        }
+
+        //public static void PagarBoleto(double valor)
+        //{
+        //    Console.WriteLine("Pago no boleto valor de " + valor);
+        //}
+      
+    }
+}
