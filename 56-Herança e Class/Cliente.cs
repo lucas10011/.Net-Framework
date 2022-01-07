fd338c4011409bbf3d48531039ad986955963faf
lucas10011 <56743482+lucas10011@users.noreply.github.com>
2022-01-07 06:49:15 -0300

    Add files via upload

---
+++ 56-Herança e Class/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GPauli.Entidades
+{
+    public class Cliente
+    {
+        // atributos
+        public int Codigo;
+        public string Nome;
+        // métodos
+        public void Mostrar()
+        {
+            Console.WriteLine("Dados do cliente: {0}-{1}", Codigo, Nome);
+        }
+    }
+
+    public class PessoaFisica : Cliente
+    {
+        public string CPF;
+        public void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("CPF: {0}", CPF);
+        }
+    }
+
+    public class PessoaJuridica : Cliente
+    {
+        public string CNPJ;
+        public void Mostrar()
+        {
+            base.Mostrar();
+            Console.WriteLine("CNPJ: {0}", CNPJ);
+        }
+    }
+
+    public class Governo : Cliente
+    {
+        public string Setor;
+    }
+}
