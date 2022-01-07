104d5d84c077f5bf9314d0e18a0b14c8d9512ae4
lucas10011 <56743482+lucas10011@users.noreply.github.com>
2022-01-07 06:52:25 -0300

    Add files via upload

---
+++ 58-Polimorfismo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pedidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pedido pedido = new Pedido(new Cartao()); // injeto a dependência
+            double valor = 2000;
+            pedido.Pagar(valor);
+            Console.ReadLine();
+        }
+    }
+}
