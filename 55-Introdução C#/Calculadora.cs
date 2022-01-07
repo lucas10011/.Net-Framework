d5b3cd2a534ffa7bb7a9288ef33ee3a235ae02e8
lucas10011 <56743482+lucas10011@users.noreply.github.com>
2022-01-07 06:48:32 -0300

    Add files via upload

---
+++ 55-Introdução C#/Calculadora.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GPauli.Util
+{
+    public class Calculadora
+    {
+        public int Somar(int x, int y)
+        {
+            int r = x + y; // business
+            return r;
+        }
+    }
+}
