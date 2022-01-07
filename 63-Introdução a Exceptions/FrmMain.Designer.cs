182ea5f6b54e8b0e1343978ecf2c408b2b9a5f3c
lucas10011 <56743482+lucas10011@users.noreply.github.com>
2022-01-07 06:58:42 -0300

    Add files via upload

---
+++ 63-Introdução a Exceptions/FrmMain.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInvocar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnInvocar
+            // 
+            this.btnInvocar.Location = new System.Drawing.Point(21, 23);
+            this.btnInvocar.Name = "btnInvocar";
+            this.btnInvocar.Size = new System.Drawing.Size(75, 23);
+            this.btnInvocar.TabIndex = 0;
+            this.btnInvocar.Text = "Invocar";
+            this.btnInvocar.UseVisualStyleBackColor = true;
+            this.btnInvocar.Click += new System.EventHandler(this.btnInvocar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(21, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(361, 225);
+            this.listBox1.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 317);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnInvocar);
+            this.Name = "FrmMain";
+            this.Text = "Exceptions";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInvocar;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 63-Introdução a Exceptions/FrmMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinApp
+{
+    public class EProgramadorException: Exception
+    {
+
+    }
+
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Estagiario(string tarefa)
+        {
+            Convert.ToDateTime(tarefa);
+        }
+
+        private void Programador(string tarefa)
+        {
+            Estagiario(tarefa);
+        }
+
+        private void Lider(string tarefa)
+        {
+            try
+            {
+                Programador(tarefa);
+            }
+            catch (FormatException E)
+            {
+                listBox1.Items.Add("Erro tratado pelo líder: " + E.Message);
+                throw new EProgramadorException();
+            }        
+        }
+
+        private void Gerente(string tarefa)
+        {
+            Lider(tarefa);
+        }
+
+        private void btnInvocar_Click(object sender, EventArgs e)
+        {
+            string tarefa = "criar aplicativo";
+            Gerente(tarefa);
+        }
+    }
+}
