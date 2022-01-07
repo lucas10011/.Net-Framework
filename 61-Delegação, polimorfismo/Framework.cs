83d4410d531f3693187020c045e4ea14e7a83a2d
lucas10011 <56743482+lucas10011@users.noreply.github.com>
2022-01-07 06:57:04 -0300

    Add files via upload

---
+++ 61-Delegação, polimorfismo/Framework.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Delegacao
+{
+    // S.O.L.I.D.
+    public class Pedido
+    {
+        // associação
+        private FormaPagamento _forma;
+
+        public void Fechar(double valor)
+        {
+            // delegação polimórfica
+            // semelhante a um padrão State
+            this._forma.Pagar(valor);
+        }
+
+        public Pedido(FormaPagamento forma)
+        {
+            // guardo internamente a forma de pagamento
+            // que foi passada no construtor da classe
+            // para que depois seja feita a delegação
+            this._forma = forma;
+        }
+    }
+
+    public abstract class FormaPagamento
+    {
+        public abstract void Pagar(double valor);
+    }
+
+    public class Boleto: FormaPagamento
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago boleto no valor de " + valor);
+        }
+    }
+
+    public class Cartao: FormaPagamento
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago cartão no valor de " + valor);
+        }
+    }
+
+    public class Cheque : FormaPagamento
+    {
+        public override void Pagar(double valor)
+        {
+            Console.WriteLine("Pago no cheque o valor de " + valor);
+        }
+    }
+
+
+}
---
+++ 61-Delegação, polimorfismo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacao
+{
+    // UI
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valor = 2500;
+            // Injeção de dependência (DI) / Inversão de Controle (IoC)
+            var pedido = new Pedido(new Cheque()); 
+            pedido.Fechar(valor);
+            Console.ReadLine();
+        }
+    }
+}
